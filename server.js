97dbd45d015bfd03d4a02a3af7abc330e19585a9
Sean Roades <seanroades@college.harvard.edu>
2023-02-09 19:52:56 -0500

    hero

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const serverless = require('serverless-http');
 require('dotenv').config();
 
 const app = express();
-// const router = express.Router();
 const port = process.env.PORT || 5001;
 
 const cors = require('cors');
